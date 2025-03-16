a9f56e11cc42f86b5c4d539b911504b9da41fb00
Victor Ren <victorren2002@gmail.com>
2025-03-15 18:34:07 -0700

    removed unnecessary code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // Proxy all requests starting with /api to your backend
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  // server: {
+  //   port: 5173,
+  //   proxy: {
+  //     // Proxy all requests starting with /api to your backend
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //       changeOrigin: true,
+  //       secure: false
+  //     }
+  //   }
+  // }
 })
